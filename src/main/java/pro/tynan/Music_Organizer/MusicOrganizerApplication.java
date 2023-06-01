53aa6703187d79571cbb39d87aff85bf2b70333f
Guenther <guenther8611@saskpolytech.ca>
2023-06-01 13:44:49 -0600

    Initial commit with Spring

---
+++ src/main/java/pro/tynan/Music_Organizer/MusicOrganizerApplication.java
@@ -0,0 +1,13 @@
+package pro.tynan.Music_Organizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/tynan/Music_Organizer/MusicOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.tynan.Music_Organizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
